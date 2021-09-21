859e2d5596c16149d0430b038de7080263df5a6b
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-09-21 13:58:09 -0400

    provided annotated manifest as documentation

---
+++ public/annotated-manifest.js
@@ -0,0 +1,24 @@
+{
+    //name of this extension follows the JS camelCase convention
+    "name": "circleBack",
+    //currently only developed to run on Chrome but exploring compatibility w/ Firefox & Brave
+    "description": "Chrome Extension for trackable LinkedIn networking",
+    "version": "1.0.0",
+    //manifest format 3 is the latest version according to Google's documentation
+    "manifest_version": 3,
+    //actions specified to customise btns that appear on Chrome toolbar,
+    //which triggers a pop-up with the contents of circleBack's index.html
+    "action" : {
+        "default_popup": "index.html",
+        "default_title": "Toggle circleBack"
+    },
+    "icons": {
+        //favicon displayed on extension's pages
+        "16" : "",
+        //icon displayed on chrome://extensions
+        "48" : "",
+        //icon displayed on chrome web store
+        "128" : ""
+    }
+
+}
