fcea92e4a98880765c0c0902bd5f7ab01152bdfb
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-09-21 15:19:19 -0400

    specified npm install vs cdn html linking

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 
 import App from "./App"
 
-// importing bootstrap as a dependency
+// importing bootstrap as a dependency vs through CDN
 import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.render(
