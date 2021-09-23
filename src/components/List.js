7bdeb4a5bd3ddbbca5926a1703642a1f18de3905
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-09-22 22:26:24 -0400

    all top-level components rendering correctly, w/o styling

---
+++ src/components/List.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+//bringing in manually recorded array of contacts
+import sampleContacts from "../contacts.js"
+
+const List = () => {
+    return (
+        <div className="listContainer">
+            <p>Item</p>
+            <p>Item</p>
+            <p>Item</p>
+            <p>Item</p>
+            <p>Item</p>
+        </div>
+    )
+}
+
+export default List
