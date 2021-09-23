b9be9ee21ca2ec5ede230874eb51093d0fe400de
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-09-22 20:55:17 -0400

    arranged App.js with top-level components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react"
 
+//importing top-level components to arrange App.js as "index":
+import Header from "./components/Header"
+import List from "./components/List"
+import Footer from "./components/Footer"
+
+
 const App = () => {
     return (
-        <>
-            <h>Placeholder</h>
-        </>
+        <div className="container col-12 col-sm-6">
+            <Header />
+            <List />
+            <Footer />
+        </div>
     )
 }
 
