8e9e19c92844c2ad861a3685afec0d22d5188081
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-09-23 14:33:05 -0400

    bootstrap 3 col layout working & divved up re: ordering

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,68 @@
 import React from "react"
 
 const Contact = props => {
+
+    // container to hold the vertical column of profile pic and company logo
+
+
+    const nameAndTitleStyle = {
+
+    }
+
+    const companyLogo = {
+        width: "65%",
+        height: "65%"
+    }
+
+    const nameStyle = {
+        padding: 2.5,
+        textDecoration: "none",
+        color: "black",
+        fontWeight: "normal",
+    }
+
     return (
-        <>
-            <p>
-                {/*hyperlink to contact page on fullName*/}
-                <a href={props.profileURL}>
+        <li className="list-group-item d-flex flex-row">
+            <div
+                className="d-flex flex-column col-4"
+            >
+                {/*profile picture as shown on in/profile link*/}
+                <img
+                    src={props.contactPhotoURL}
+                    alt={props.fullName}
+                    className="order-0"
+
+                />
+
+                {/*company logo*/}
+                <img
+                    style={companyLogo}
+                    src={props.companyLogoURL}
+                    alt={props.fullName}
+                    className="order-1"
+
+                />
+
+            </div>
+            {/*hyperlink to profile link on fullName*/}
+            <div
+                className="d-flex flex-column col-8"
+            >
+                <a
+                    style={nameStyle}
+                    className="d-flex align-items-start"
+                    href={props.profileURL}
+                >
                     {props.name}
                 </a>
+            </div>
 
-            </p>
-        </>
+            <div
+                className="d-flex flex-column col-5"
+            >
+
+            </div>
+        </li>
     )
 }
 
