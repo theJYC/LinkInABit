0e44632ae5bafcbea4b9fc1aeeb08f98e12188c5
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-09-22 21:56:45 -0400

    chrome.storage likely calls for nested objects but initialised sample array for now

---
+++ src/contacts.js
@@ -0,0 +1,44 @@
+//initialising a sample array of contact objects for UI development:
+//in prod, sampleContacts will replicate chrome.storage data that user populates.
+
+const sampleContacts = [
+    {
+        id: 1,
+        fullName: "Jin Hyuk Choi",
+        companyName: "SVB Leerink",
+        profileURL: "https://www.linkedin.com/in/jinhyuk-ryan-choi/",
+        contactInfoLink: "https://www.linkedin.com/in/jinhyuk-ryan-choi/detail/contact-info/",
+        currentTitle: "Investment Banking Analyst",
+        companyLinkedInURL: "https://www.linkedin.com/company/kaikustartups/"
+    },
+    {
+        id: 2,
+        fullName: "Fredrick Thompson",
+        companyName: "Duolingo",
+        profileURL: "https://www.linkedin.com/in/fredrick-thompson-875520174/",
+        contactInfoLink: "https://www.linkedin.com/in/fredrick-thompson-875520174/detail/contact-info/",
+        currentTitle: "Product Manager",
+        companyLinkedInURL: "https://www.linkedin.com/company/duolingo/"
+    },
+    {
+        id: 3,
+        fullName: "Marco Fayet",
+        companyName: "United Nations",
+        profileURL: "https://www.linkedin.com/in/marcofayet/",
+        contactInfoLink: "https://www.linkedin.com/in/marcofayet/detail/contact-info/",
+        currentTitle: "Data Consultant",
+        companyLinkedInURL: "https://www.linkedin.com/company/united-nations/"
+    },
+    {
+        id: 4,
+        fullName: "Oliver Whittam",
+        companyName: "Deloitte",
+        profileURL: "https://www.linkedin.com/in/oliverwhittam/",
+        contactInfoLink: "https://www.linkedin.com/in/oliverwhittam/contact-info/",
+        currentTitle: "PWM Analyst",
+        companyLinkedInURL: "https://www.linkedin.com/company/deloitte/"
+    }
+
+]
+
+export default sampleContacts
