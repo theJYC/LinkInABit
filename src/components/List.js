d3d9e01df505c0704ab5990c17d6c0b1483de1a6
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-10-10 17:19:35 -0400

    refactored List.js to modify "contacts" to "profiles" for coherence

--- src/components/List.js
+++ src/components/List.js
@@ -2,41 +2,41 @@ import React from "react"
 import PropTypes from "prop-types"
 
 //Contact component used as template for each profile:
-import Contact from "./Contact"
+import Profile from "./Profile"
 
 
-const List = props => {
+const List = ({profiles}) => {
 
     // mapped out using the key-value attributes of the contacts.js array of objs:
-    const contactsComponents = props.contacts.map(contact => {
+    const profilesComponents = profiles.map(profile => {
         return (
-            <Contact
+            <Profile
                 /* paying special attention to ordering of props */
                 /* as order of appearance/usage in Contact */
-                key={contact.id}
-                name={contact.fullName}
-                company={contact.companyName}
-                contactPhotoURL={contact.contactPhotoURL}
-                companyLogoURL={contact.companyLogoURL}
-                profileURL={contact.profileURL}
-                contactInfo={contact.contactInfoLink}
-                currentTitle={contact.currentTitle}
-                companyLinkedIn={contact.companyLinkedInURL}
+                key={profile.id}
+                name={profile.fullName}
+                company={profile.companyName}
+                contactPhotoURL={profile.contactPhotoURL}
+                companyLogoURL={profile.companyLogoURL}
+                profileURL={profile.profileURL}
+                contactInfo={profile.contactInfoLink}
+                currentTitle={profile.currentTitle}
+                companyLinkedIn={profile.companyLinkedInURL}
             />
         )
     })
 
     return (
         <ul className="d-flex flex-column-reverse list-group">
-            {/* no. of objs will be dynamically updated and mapped in contactsComponents */}
-            {contactsComponents}
+            {/* no. of objs will be dynamically updated and mapped in profilesComponents */}
+            {profilesComponents}
         </ul>
     )
 }
 
 //validating that List is receiving "contacts" prop, which is of the array data type, and required:
 List.propTypes = {
-    contacts: PropTypes.array.isRequired
+    profiles: PropTypes.array.isRequired
 }
 
 //note to self: make sure to modify ^ when chrome API message passing is integrated
